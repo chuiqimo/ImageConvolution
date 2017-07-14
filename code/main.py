c74fa12bf4cd3b2fa14ef512275376e921046d63
SongYihang <songyihang@dhcp-206-12-52-103.eduroam.wireless.ubc.ca>
2017-07-13 17:36:45 -0700

    with folders

--- code/main.py
+++ code/main.py
@@ -25,35 +25,37 @@ if __name__ == '__main__':
         print(Z)
 
     if question == '2':
-        X = imread('Gothic.jpg', flatten=True)
-        imsave('grey.jpg', X)
+        X = imread('../figs/Gothic.jpg', flatten=True)
+        imsave('../figs/grey.jpg', X)
         W = np.array([[1,2,0]
         	         ,[2,0,-2]
         	         ,[0,-2,-1]])
         grad = signal.convolve2d(X, W)
-        imsave('outfile.jpg', grad)
+        imsave('../figs/emboss.jpg', grad)
 
     if question == '3':
-        X = imread('Gothic.jpg')
+        X = imread('../figs/Gothic.jpg')
         N,D,r = X.shape
         Z = np.zeros((N,D,r))
         Z = X
         X1 = X[:,:,0]
         X2 = X[:,:,1]
         X3 = X[:,:,2]
+
+        # high varience on red channel
         b1 = gaussian_filter(X1, sigma=2)
         b2 = gaussian_filter(X2, sigma=7)
         b3 = gaussian_filter(X3, sigma=2)
 
         Z[:,:,0] = b1
 
-        imsave('Gaussian1.jpg', Z)
+        imsave('../figs/Gaussian1.jpg', Z)
 
         Z[:,:,1] = b2
-        imsave('Gaussian2.jpg', Z)
+        imsave('../figs/Gaussian2.jpg', Z)
 
         Z[:,:,2] = b3
-        imsave('Gaussian.jpg', Z)
+        imsave('../figs/Gaussian.jpg', Z)
 
     if question == '4':
-        X = imread('Gothic.jpg')
+        X = imread('../figs/Gothic.jpg')
